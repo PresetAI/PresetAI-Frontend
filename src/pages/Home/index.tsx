4e6205f4848797af5299580570c7971bbe7bd98b
ZanyuanYang <59616360+ZanyuanYang@users.noreply.github.com>
2023-06-28 12:23:39 -0700

    feat: add homepage ui

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,38 @@
+import {
+  ArrowPathIcon,
+  ChevronRightIcon,
+  CloudArrowUpIcon,
+  Cog6ToothIcon,
+  FingerPrintIcon,
+  LockClosedIcon,
+  ServerIcon,
+} from '@heroicons/react/20/solid';
+import {
+  BoltIcon,
+  CalendarDaysIcon,
+  UsersIcon,
+} from '@heroicons/react/24/outline';
+import React from 'react';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import LogoCloud from './components/LogoCloud';
+import Feature from './components/Feature';
+import Stats from './components/Stats';
+import CTA from './components/CTA';
+import { Footer } from '../../layouts';
+
 function Home() {
-  return <h1>home</h1>;
+  return (
+    <main className="bg-gray-900 block">
+      <Header />
+      <Hero />
+      <LogoCloud />
+      <Feature />
+      <Stats />
+      <CTA />
+      <Footer />
+    </main>
+  );
 }
 
 export default Home;
