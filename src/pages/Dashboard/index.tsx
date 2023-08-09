8492ad8f8b86390550da97f986fb0e40c612fffb
ZanyuanYang <59616360+ZanyuanYang@users.noreply.github.com>
2023-08-09 13:34:14 -0700

    feat: chatbot

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,42 @@
 import React from 'react';
 import { Sidebar } from '@/layouts';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 
 function Dashboard() {
-  return <Sidebar projectId="123" component={null} />;
+  const markdown = `A paragraph with *emphasis* and **strong importance**.
+
+> A block quote with ~strikethrough~ and a URL: https://reactjs.org.
+
+* Lists
+* [ ] todo
+* [x] done
+
+A table:
+
+| a | b |
+| - | - |
+
+\`\`\`js
+console.log('Hello, world!');
+\`\`\`
+
+`;
+  return (
+    <Sidebar
+      projectId="123"
+      component={
+        <div>
+          <h1>Dashboard</h1>
+          <ReactMarkdown
+            remarkPlugins={[[remarkGfm, { singleTilde: false }]]}
+            className="prose lg:prose-xl"
+          >
+            {markdown}
+          </ReactMarkdown>
+        </div>
+      }
+    />
+  );
 }
 export default Dashboard;
